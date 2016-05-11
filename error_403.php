2d07362f1026f0c0a610d390631419c93fc79e30
emintela <ewmintela@gmail.com>
2016-05-11 16:42:33 -0400

    Add files via upload

---
+++ error_403.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>403 Forbidden Error</title>
+    <style type="text/css">
+        body{margin: 0px; color: white;}
+        #fof{margin:auto; display:block; position:relative; width:100%; height:100%; position: fixed; background:url("/img/fail_whale.png") top left no-repeat; line-height:1.6em; text-align:center;background-size: 100%}
+        #fof .positioned{display:block; position:absolute; top:150px; right:0; width:768px; height:450px;font-family: Helvetica, Verdana, Arial;}
+        #fof .hgroup{margin-bottom:50px;}
+        #fof .hgroup h1{}
+        #fof .hgroup h2{}
+        #fof img{margin-bottom:25px;}
+        #fof p{display:block; margin:0 0 6px 0; padding:0; font-size:16px;}
+        #fof #respond input{width:200px; padding:5px; border:1px solid #CCCCCC;}
+    </style>
+</head>
+<body>
+<div class="wrapper row2">
+    <div id="container" class="clear">
+
+        <section id="fof" class="clear">
+            <div class="positioned">
+
+                <div class="hgroup">
+                    <h1>Ooops...</h1>
+                    <h2>Something sanapped.</h2>
+                </div>
+                <p>The request was unsuccessful because you are trying to access a invalid file format.</p>
+                <div id="respond">
+                    <p>You can report it at the <a href="http://learnsauce.com/knowledgebase" target="_blank">Learnsauce Knowledgebase</a></p>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
+</div>
+</body>
+</html>
